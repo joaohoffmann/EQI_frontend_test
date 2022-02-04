41590f6353267a854063dad77a8f9fec477b0d48
João Pedro Hoffmann <joao.pedro.jordao09@gmail.com>
2022-02-04 19:51:08 -0300

    quarto commit

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const indicadores = "http://localhost:3000/indicadores"
 const simulacoes = "http://localhost:3000/simulacoes"
 
-function getSimulacoes() {
-  axios.get(simulacoes)
+
+onload(getIndicadores())
+
+
+function getIndicadores() {
+  axios.get(indicadores)
   .then(response => {
-    let data = response.data 
-    conteudo.innerHTML = JSON.stringify(data)
-  })
+    const data = response.data
+    const cdi = data.slice(0,1)
+    const ipca = data.slice(1)
+    const cdiValor = cdi.map((item)=>{
+      return CDI.value = `${item.valor}%`
+    })
+    const ipcaValor = ipca.map((item)=>{
+      return IPCA.value = `${item.valor}%`
+    })
+
+     
+    })
   .catch(error => console.error(error))
 }
-getSimulacoes()
